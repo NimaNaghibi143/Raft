f1fbd098b8c6c55a20c8d7e6713b2de96968e537
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-05-01 11:26:03 +0330

    feat: implment encodeFunc to encode user cmds passed into the the state machine

--- goraft/cmd/kvapi/main.go
+++ goraft/cmd/kvapi/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"sync"
 )
@@ -40,4 +42,30 @@ func (s *statemachine) Apply(cmd []byte) ([]byte, error) {
 	}
 
 	return nil, nil
+}
+
+// cmds passed from the user into the state machine need to be serialized to bytes.
+
+func encodeCommand(c command) []byte {
+	msg := bytes.NewBuffer(nil)
+	err := msg.WriteByte(uint8(c.kind))
+	if err != nil {
+		panic(err)
+	}
+
+	err = binary.Write(msg, binary.LittleEndian, uint64(len(c.key)))
+	if err != nil {
+		panic(err)
+	}
+
+	msg.WriteString(c.key)
+
+	err = binary.Write(msg, binary.LittleEndian, uint64(len(c.value)))
+	if err != nil {
+		panic(err)
+	}
+
+	msg.WriteString(c.value)
+
+	return msg.Bytes()
 }
